ad08549a14746d1d9f97390fdbcb88210bc1377b
najongjine <najongjin3@hotmail.com>
2025-11-27 11:27:44 +0900

    .

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as types from "../types/types";
 
 export default function HomeScreen() {
+  const [memos, setMemos] = useState<types.Memo[]>([]);
+
   return (
     <View>
       <Text>메모 리스트</Text>
